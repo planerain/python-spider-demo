89223c66c264516c92e354f4d6a46777d7124a28
planerain <172662169@qq.com>
2019-03-16 14:34:05 +0800

    爬取墨迹天气

---
+++ weather.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+def get_weather_data():
+    url = 'https://tianqi.moji.com/weather/china/shaanxi/xian'
+    res = requests.get(url)
+    html_doc = res.text
+    # print(html_doc)
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    weather_info = soup.find('div', {'class': 'wea_weather clearfix'})
+    # print(weather_info)
+    temperature = weather_info.find('em').get_text()
+    # print(temperature)
+    weather = weather_info.find('b').get_text()
+    # print(weather)
+    update_time = weather_info.find('strong').get_text()[2:]
+    AQI = soup.find('div', {'class': 'wea_alert clearfix'}).find('em').get_text()
+    # print(AQI)
+    about = soup.find('div', {'class': 'wea_about clearfix'})
+    # print(about)
+    humidity = about.find('span').get_text().split(' ')[1]
+    speed = about.find('em').get_text()
+
+    info_all = '墨迹天气提醒您\n' + '今天是{0}年{1}年{2}日,陕西省西安市的天气({3})为：\n天气:{4}\n实时温度:{5}\n空气质量指数:{6}\n湿度:{7}\n风速:{8}'.format(
+        str(datetime.date.today()).split('-')[0],
+        str(datetime.date.today()).split('-')[1],
+        str(datetime.date.today()).split('-')[2],
+        update_time,
+        weather,
+        temperature + '℃', AQI,
+        humidity, speed)
+    print(info_all)
+
+
+if __name__ == '__main__':
+    get_weather_data()
